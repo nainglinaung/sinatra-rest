6dd6db5b76c1a3ad150e93c671d4078f80bca980
Ye Lin Aung <me@yelinaung.com>
2014-08-21 16:09:57 +0630

    remove trailing white spaces from configuration.rb

--- configuration.rb
+++ configuration.rb
@@ -1,39 +1,31 @@
 require 'json'
 
+class MongoAccessor
 
-class MongoAccessor 
+  def initialize(hostname,port)
+    @client =  Mongo::MongoClient.new(hostname,port)
+  end
 
-	def initialize(hostname,port)
-		@client =  Mongo::MongoClient.new(hostname,port)
-	end
-	
-	def list(database,collectionName)
-		db = @client.db(database)
-		db[collectionName].find.to_a.to_json
-	end
+  def list(database,collectionName)
+    db = @client.db(database)
+    db[collectionName].find.to_a.to_json
+  end
 
+  def findById(database,collectionName,id) 
+    db = @client.db(database)
+    db[collectionName].find('_id' => BSON::ObjectId.from_string(id)).to_a.to_json
+  end 
 
-	def findById(database,collectionName,id) 
-	 	db = @client.db(database)
-		db[collectionName].find('_id' => BSON::ObjectId.from_string(id)).to_a.to_json
-	end 
+  def updateById(database,collectionName,id)
+    db = @client.db(database)
+    db[collectionName].update({'_id' => BSON::ObjectId.from_string(id)},{"$set" => {"welcome" => "Hello There"}})
+    "[{condition :'success'}]"
+  end	
 
-
-	def updateById(database,collectionName,id)
-	 	db = @client.db(database)
-		db[collectionName].update({'_id' => BSON::ObjectId.from_string(id)},{"$set" => {"welcome" => "Hello There"}})
-		"[{condition :'success'}]"
-	end	
-
-	def deleteById(database,collectionName,id)
-		db = @client.db(database)
-		db[collectionName].remove('_id' => BSON::ObjectId.from_string(id))
-		"[{condition :'success'}]"
-	end
+  def deleteById(database,collectionName,id)
+    db = @client.db(database)
+    db[collectionName].remove('_id' => BSON::ObjectId.from_string(id))
+    "[{condition :'success'}]"
+  end
 
 end 	
-
-
-
-
-
