3e8454486227398450950c5f610a0f348d0c518b
Ye Lin Aung <me@yelinaung.com>
2014-08-21 16:37:41 +0630

    no more offenses

--- index.rb
+++ index.rb
@@ -5,34 +5,34 @@ require './model.rb'
 get '/:db/:collection/' do
   headers \
   'Content-Type' => 'application/json'
-  get = params[:captures]  
-  body list get[0],get[1]
+  get = params[:captures]
+  body list get[0], get[1]
 end
 
 post '/:db/:collection/' do
   headers \
   'Content-Type' => 'application/json'
-  create params 
+  create params
   'Hello World'
-end 
+end
 
 get '/:db/:collection/:id/' do
   headers \
   'Content-Type' => 'application/json'
   get = params[:captures]
-  body read get[0],get[1],get[2] 
-end 
+  body read get[0], get[1], get[2]
+end
 
 put '/:db/:collection/:id/' do
   headers \
-  "Content-Type" => 'application/json'
-  get = params[:captures] 
-  body update get[0],get[1],get[2]
-end 
+  'Content-Type' => 'application/json'
+  get = params[:captures]
+  body update get[0], get[1], get[2]
+end
 
 delete '/:db/:collection/:id/' do
   headers \
   'Content-Type' => 'application/json'
-  get = params[:captures]  
-  body delete get[0],get[1],get[2]
-end 
+  get = params[:captures]
+  body delete get[0], get[1], get[2]
+end
