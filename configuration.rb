d5fdcca418c4c2106cda327153bbf28dfbfc9dc0
Ye Lin Aung <me@yelinaung.com>
2014-08-21 16:18:55 +0630

    more formatting

--- configuration.rb
+++ configuration.rb
@@ -1,7 +1,6 @@
 require 'json'
 
 class MongoAccessor
-
   def initialize(hostname, port)
     @client =  Mongo::MongoClient.new(hostname, port)
   end
@@ -11,14 +10,14 @@ class MongoAccessor
     db[collection_name].find.to_a.to_json
   end
 
-  def find_by_id(database, collection_name,id) 
+  def find_by_id(database, collection_name, id)
     db = @client.db(database)
     db[collection_name].find('_id' => BSON::ObjectId.from_string(id)).to_a.to_json
-  end 
+  end
 
   def update_by_id(database, collection_name, id)
     db = @client.db(database)
-    db[collection_name].update({'_id' => BSON::ObjectId.from_string(id)}, {"$set" => {"welcome" => "Hello There" } })
+    db[collection_name].update({ '_id' => BSON::ObjectId.from_string(id) }, { '$set' => { 'welcome' => 'Hello There' } })
     "[{condition :'success'}]"
   end
 
