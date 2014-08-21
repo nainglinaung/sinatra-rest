0e737acbd5f36119b22d8693e6f4728c0342f8ba
Ye Lin Aung <me@yelinaung.com>
2014-08-21 16:32:34 +0630

    remove trailing spaces again

--- index.rb
+++ index.rb
@@ -2,45 +2,37 @@ require 'mongo'
 require 'sinatra'
 require './model.rb'
 
-
-
-get '/:db/:collection/' do 
+get '/:db/:collection/' do
   headers \
-    "Content-Type" => "application/json"
+  "Content-Type" => "application/json"
   get = params[:captures]  
   body list get[0],get[1]
-  
-  
 end
 
-post '/:db/:collection/'  do
-	headers \
-	 "Content-Type" => "application/json"
-	create params 
-	  "Hello World"
+post '/:db/:collection/' do
+  headers \
+  "Content-Type" => "application/json"
+  create params 
+  "Hello World"
 end 
 
-get '/:db/:collection/:id/' do 
-	headers \
-	 "Content-Type" => "application/json"
-	get = params[:captures]
-	body read get[0],get[1],get[2] 
+get '/:db/:collection/:id/' do
+  headers \
+  "Content-Type" => "application/json"
+  get = params[:captures]
+  body read get[0],get[1],get[2] 
 end 
 
 put '/:db/:collection/:id/' do
-	headers \
-	 "Content-Type" => "application/json"
-	get = params[:captures] 
-	body update get[0],get[1],get[2]
-	
+  headers \
+  "Content-Type" => "application/json"
+  get = params[:captures] 
+  body update get[0],get[1],get[2]
 end 
 
 delete '/:db/:collection/:id/' do
-	headers \
-	 "Content-Type" => "application/json"
-	get = params[:captures]  
-	body delete get[0],get[1],get[2]
+  headers \
+  "Content-Type" => "application/json"
+  get = params[:captures]  
+  body delete get[0],get[1],get[2]
 end 
-
-
-
